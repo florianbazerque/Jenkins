b6f0089b103b27d6fb255ad0bc7528e0f627b607
florianbazerque <florian.bazerque@orange.fr>
2018-03-22 10:40:47 +0100

    Update MathTest.php

--- test/MathTest.php
+++ test/MathTest.php
@@ -5,10 +5,12 @@ use PHPUnit\Framework\TestCase;
 class MathTest extends TestCase
 {
 
+	//Test la fonction double de la class Math
 	public function testDouble(){
 		$this->assertEquals(4, \Grafikart\Math::double(2));
 	}
 
+	//Test la fonction double de la class Math avec une valeur de 0
 	public function testDoubleIfZero(){
 		$this->assertEquals(0, \Grafikart\Math::double(0));
 	}
